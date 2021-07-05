dedcaa2cdc93183e9c9ba7615ad9ddf0f19fa60f
Maritza <maritza.bello@islas.org.mx>
2021-07-05 11:10:35 -0700

    ➕ Add python dependencies

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dummy_transformations",
+    name="geci_plots",
     version="0.1.0",
-    packages=["dummy_transformations"],
+    packages=["geci_plots"],
+    install_requires = [
+        "numpy",
+        "pandas",
+        "matplotlib",
+    ],
     author="Fernando Alvarez",
     python_requires='>=3.6'
 )
