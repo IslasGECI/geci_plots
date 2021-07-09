935398bd134fb72bba49cb37e104c66cd53b9ca8
Nepo Rojas <braulio.rojas@islas.org.mx>
2021-07-09 11:14:57 -0700

    ✅ Add some tests

--- tests/test_geci_plots.py
+++ tests/test_geci_plots.py
@@ -1,6 +1,11 @@
-from geci_plots import create_box_plot_data
 import numpy as np
 import pandas as pd
+from geci_plots import (
+    create_box_plot_data,
+    historic_mean_effort,
+    filter_by_season_and_zone,
+    ticks_positions_array,
+)
 
 
 def test_create_box_plot_data():
@@ -15,3 +20,22 @@ def test_create_box_plot_data():
     np.testing.assert_array_equal(obtained_seasons, expected_seasons)
     for i in range(3):
         pd.testing.assert_series_equal(obtained_box_plot_data[i], expected_box_plot_data[i])
+
+
+def test_historic_mean_effort():
+    test_for_data = {"Season": np.array([1, 1, 1, 1]), "Zone": [2, 2, 2, 2]}
+    obtained_mean = historic_mean_effort(test_for_data, "Season")
+    expected_mean = 1.0
+    assert expected_mean == obtained_mean
+
+
+def test_filter_by_season_and_zone():
+    test_for_data = pd.DataFrame({"Season": np.array([1, 1, 1, 1]), "Zone": [2, 2, 2, 2]})
+    obtained_data = filter_by_season_and_zone(test_for_data, 1, 2)
+
+
+def test_ticks_positions_array():
+    test_tick = [1, 2, 3]
+    expected_tick_position = np.array([1.0, 2.0, 3.05])
+    obtained_tick_position = ticks_positions_array(test_tick)
+    np.testing.assert_equal(expected_tick_position, obtained_tick_position)
