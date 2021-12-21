✨ Change correct y limit for scaling

geci_plots/geci_plots.py
@@ -358,7 +358,7 @@ def annotate_pie_chart(ax, wedges, box_labels, scale_x=1.35, scale_y=1.4, fontsi
         x,y = calculate_anotations_positions_for_wedges_2(central_angle)
         x_sign = np.sign(x)
         horizontalalignment = {-1: "right", 1: "left"}[int(x_sign)]
-        vertical_scaling = {True: -1, False: +1}[y <= 0.5]
+        vertical_scaling = {True: -1, False: +1}[y <= 0]
         connectionstyle = "angle,angleA=0,angleB={}".format(central_angle)
         kw["arrowprops"].update({"connectionstyle": connectionstyle})
         ax.annotate(