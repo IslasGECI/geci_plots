a075ddc8497a87dad270d6408a03c6e9e6463717
Xavier Paredes <jxavierpp1@gmail.com>
2022-01-24 21:02:39 +0000

    🚧 Add styles to python package

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="geci_plots",
     version="0.4.1",
     packages=["geci_plots"],
+    package_data={'': ['geci_styles.mplstyle']},
+    include_package_data=True,
     install_requires=[
         "matplotlib",
         "numpy",
