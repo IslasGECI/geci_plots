59eafd19bff2c6bed6e4547103fb03601ab44c87
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-24 18:32:14 +0000

    ♻️  Refactor

--- tests/test_plot_kernel_density_gls.py
+++ tests/test_plot_kernel_density_gls.py
@@ -1,6 +1,10 @@
 import geci_test_tools as gtt
 
-from geci_plots.plot_kernel_density_gls import _plot_kernel_density_gls
+from geci_plots.plot_kernel_density_gls import (
+    adapt_gls_data,
+    _plot_kernel_density_gls,
+    X_plot_kernel_density_gls,
+)
 
 
 def test_plot_kernel_density_gls():
@@ -11,8 +15,9 @@ def test_plot_kernel_density_gls():
     result_map_path = "tests/kernel_50_percent_gls_albatros.png"
 
     gtt.if_exist_remove(result_map_path)
-    _plot_kernel_density_gls(
-        gls_data_path, global_shapefile_data_path, path_rose_wind, selected_contour, result_map_path
+    gls_data = adapt_gls_data(gls_data_path)
+    X_plot_kernel_density_gls(
+        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, result_map_path
     )
     gtt.assert_exist(result_map_path)
 
