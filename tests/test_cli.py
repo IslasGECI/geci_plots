1f2bfe3b649cf10dedf76120520aeae91dff1f0f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-24 17:56:40 +0000

    🛑🧪 Fail tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,3 +10,10 @@ runner = CliRunner()
 def test_plot_kernel_density_gls():
     result = runner.invoke(cli, ["plot-kernel-density-gls", "--help"])
     assert result.exit_code == 0
+
+
+def test_version():
+    result = runner.invoke(cli, ["version"])
+    assert result.exit_code == 0
+    expected_version = "0.4.1"
+    assert result.stdout == expected_version
