092583006a1f98f14eb9945abf0b27090eb1051f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-24 19:09:32 +0000

    ♻️  Refactor

--- tests/test_plot_kernel_density_gls.py
+++ tests/test_plot_kernel_density_gls.py
@@ -1,5 +1,5 @@
 import geci_test_tools as gtt
-from geci_plots import plt
+import matplotlib as plt
 
 from geci_plots.plot_kernel_density_gls import (
     adapt_gls_data,
@@ -12,13 +12,12 @@ def test_plot_kernel_density_gls():
     global_shapefile_data_path = "tests/data/division_politica_paises.shp"
     path_rose_wind = "tests/data/rosewind.png"
     selected_contour = "50_contour"
-    result_map_path = "tests/kernel_50_percent_gls_albatros.png"
 
-    gtt.if_exist_remove(result_map_path)
     gls_data = adapt_gls_data(gls_data_path)
-    _plot_kernel_density_gls(gls_data, global_shapefile_data_path, path_rose_wind, selected_contour)
-    plt.savefig(result_map_path)
-    gtt.assert_exist(result_map_path)
+    obtained = _plot_kernel_density_gls(
+        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour
+    )
+    assert isinstance(obtained, plt.axes._axes.Axes)
 
 
 def test_plot_kernel_density():
@@ -26,10 +25,9 @@ def test_plot_kernel_density():
     global_shapefile_data_path = "tests/data/division_politica_paises.shp"
     path_rose_wind = "tests/data/rosewind.png"
     selected_contour = "50_contour"
-    result_map_path = "tests/kernel_50_percent_gps_albatros.png"
 
-    gtt.if_exist_remove(result_map_path)
     gls_data = adapt_gls_data(gls_data_path)
-    _plot_kernel_density_gls(gls_data, global_shapefile_data_path, path_rose_wind, selected_contour)
-    plt.savefig(result_map_path)
-    gtt.assert_exist(result_map_path)
+    obtained = _plot_kernel_density_gls(
+        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour
+    )
+    assert isinstance(obtained, plt.axes._axes.Axes)
