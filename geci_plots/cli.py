5b53d61a1968cece150260c104e4581af0a08ac3
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-24 18:18:27 +0000

    ✅ Pass tests

--- geci_plots/cli.py
+++ geci_plots/cli.py
@@ -1,3 +1,4 @@
+from geci_plots.plot_kernel_density_gls import _plot_kernel_density_gls
 import pandas as pd
 import typer
 import matplotlib.pyplot as plt
@@ -6,8 +7,16 @@ cli = typer.Typer()
 
 
 @cli.command()
-def plot_kernel_density_gls():
-    pass
+def plot_kernel_density_gls(
+    gls_data_path: str = typer.Option(),
+    global_shapefile_data_path: str = typer.Option(),
+    path_rose_wind: str = typer.Option(),
+    selected_contour: str = typer.Option(),
+    result_map_path: str = typer.Option(),
+):
+    _plot_kernel_density_gls(
+        gls_data_path, global_shapefile_data_path, path_rose_wind, selected_contour, result_map_path
+    )
 
 
 @cli.command()
