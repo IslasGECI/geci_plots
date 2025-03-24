d7bf247f6f2421cc49054c0eae005a4ce017a02b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-24 19:14:01 +0000

    ♻️  Refactor

--- tests/test_plot_kernel_density_gls.py
+++ tests/test_plot_kernel_density_gls.py
@@ -1,9 +1,10 @@
-import matplotlib as plt
-
 from geci_plots.plot_kernel_density_gls import (
     adapt_gls_data,
     _plot_kernel_density_gls,
 )
+
+import matplotlib as plt
+import pandas as pd
 
 
 def test_plot_kernel_density_gls():
@@ -12,7 +13,7 @@ def test_plot_kernel_density_gls():
     path_rose_wind = "tests/data/rosewind.png"
     selected_contour = "50_contour"
 
-    gls_data = adapt_gls_data(gls_data_path)
+    gls_data = pd.read_csv(gls_data_path)
     obtained = _plot_kernel_density_gls(
         gls_data, global_shapefile_data_path, path_rose_wind, selected_contour
     )
@@ -26,6 +27,7 @@ def test_plot_kernel_density():
     selected_contour = "50_contour"
 
     gls_data = adapt_gls_data(gls_data_path)
+    gls_data = gls_data.sample(frac=0.1)
     obtained = _plot_kernel_density_gls(
         gls_data, global_shapefile_data_path, path_rose_wind, selected_contour
     )
