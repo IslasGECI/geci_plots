07f09b991f9d4bd02b73e1b47cabc68b994de829
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-25 18:51:41 +0000

    ♻️  Refactor

--- tests/test_plot_kernel_density_gls.py
+++ tests/test_plot_kernel_density_gls.py
@@ -7,15 +7,15 @@ from geci_plots.plot_kernel_density_gls import (
 import matplotlib as plt
 import pandas as pd
 
+
+global_shapefile_data_path = "tests/data/division_politica_paises.shp"
+path_rose_wind = "tests/data/rosewind.png"
+selected_contour = "50_contour"
 bandwidth = 0.04
 
 
 def test_plot_kernel_density_and_gls_points():
     gls_data_path = "tests/data/gls_albatros_tests.csv"
-    global_shapefile_data_path = "tests/data/division_politica_paises.shp"
-    path_rose_wind = "tests/data/rosewind.png"
-    selected_contour = "50_contour"
-
     gls_data = pd.read_csv(gls_data_path)
     obtained = _plot_kernel_density_and_points(
         gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
@@ -25,10 +25,6 @@ def test_plot_kernel_density_and_gls_points():
 
 def test_plot_kernel_density_and_points():
     gls_data_path = "tests/data/trips_geographic_points_tests.csv"
-    global_shapefile_data_path = "tests/data/division_politica_paises.shp"
-    path_rose_wind = "tests/data/rosewind.png"
-    selected_contour = "50_contour"
-
     gls_data = adapt_gls_data(gls_data_path)
     obtained = _plot_kernel_density_and_points(
         gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
@@ -40,10 +36,6 @@ def test_plot_kernel_density_and_points():
 
 def test_plot_kernel_density():
     gls_data_path = "tests/data/trips_geographic_points_tests.csv"
-    global_shapefile_data_path = "tests/data/division_politica_paises.shp"
-    path_rose_wind = "tests/data/rosewind.png"
-    selected_contour = "50_contour"
-
     gls_data = adapt_gls_data(gls_data_path)
     obtained = _plot_kernel_density(
         gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
