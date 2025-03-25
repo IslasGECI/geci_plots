43afefe5ec8a21240692dbc4e93e4de2c590653f
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-25 19:15:45 +0000

    ✅ Pass tests

--- geci_plots/plot_kernel_density_gls.py
+++ geci_plots/plot_kernel_density_gls.py
@@ -44,6 +44,17 @@ def _plot_kernel_density(
     return ax
 
 
+def _plot_geographic_points(gls_data, global_shapefile_data_path, path_rose_wind):
+    gls_data_translated = translate_positive_longitudes(gls_data)
+
+    fig, ax = plt.subplots(figsize=(14.3, 10.4))
+    format_plot(ax)
+    plot_global_politic_division(global_shapefile_data_path, ax)
+    plot_geographic_points(gls_data_translated)
+    plot_windrose(path_rose_wind, fig)
+    return ax
+
+
 def translate_positive_longitudes(gls_data):
     mask_datos_trand = gls_data.longitude > 0
     gls_data.loc[mask_datos_trand, "longitude"] = gls_data[mask_datos_trand]["longitude"] - 360
