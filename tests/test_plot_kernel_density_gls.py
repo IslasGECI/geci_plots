2dfcf1ad9589d4c2bd5a10891ad9f92f55efffea
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-25 19:03:25 +0000

    🛑🧪 Fail tests

--- tests/test_plot_kernel_density_gls.py
+++ tests/test_plot_kernel_density_gls.py
@@ -2,6 +2,7 @@ from geci_plots.plot_kernel_density_gls import (
     adapt_gls_data,
     _plot_kernel_density,
     _plot_kernel_density_and_points,
+    _plot_geographic_points
 )
 
 import matplotlib as plt
@@ -12,10 +13,11 @@ global_shapefile_data_path = "tests/data/division_politica_paises.shp"
 path_rose_wind = "tests/data/rosewind.png"
 selected_contour = "50_contour"
 bandwidth = 0.04
+gls_data_path = "tests/data/gls_albatros_tests.csv"
+gps_data_path = "tests/data/trips_geographic_points_tests.csv"
 
 
 def test_plot_kernel_density_and_gls_points():
-    gls_data_path = "tests/data/gls_albatros_tests.csv"
     gls_data = pd.read_csv(gls_data_path)
     obtained = _plot_kernel_density_and_points(
         gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
@@ -24,10 +26,9 @@ def test_plot_kernel_density_and_gls_points():
 
 
 def test_plot_kernel_density_and_points():
-    gls_data_path = "tests/data/trips_geographic_points_tests.csv"
-    gls_data = adapt_gls_data(gls_data_path)
+    gps_data = adapt_gls_data(gps_data_path)
     obtained = _plot_kernel_density_and_points(
-        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
+        gps_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
     )
     assert isinstance(obtained, plt.axes._axes.Axes)
     assert len(obtained.collections) == 3
@@ -35,12 +36,20 @@ def test_plot_kernel_density_and_points():
 
 
 def test_plot_kernel_density():
-    gls_data_path = "tests/data/trips_geographic_points_tests.csv"
-    gls_data = adapt_gls_data(gls_data_path)
+    gps_data = adapt_gls_data(gps_data_path)
     obtained = _plot_kernel_density(
-        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
+        gps_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
     )
     plt.pyplot.savefig("kernel.png")
     assert isinstance(obtained, plt.axes._axes.Axes)
     assert len(obtained.collections) == 3
     assert len(obtained.lines) == 0
+
+
+def test_plot_geographic_points():
+    gls_data = pd.read_csv(gls_data_path)
+    obtained = _plot_geographic_points(gls_data, global_shapefile_data_path, path_rose_wind)
+    plt.pyplot.savefig("points.png")
+    assert isinstance(obtained, plt.axes._axes.Axes)
+    assert len(obtained.collections) == 3
+    assert len(obtained.lines) == 1
