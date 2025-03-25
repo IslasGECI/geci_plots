c60430c0a12f7b56f343b076dd75ab7a8e4ea660
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-25 17:12:54 +0000

    ♻️  Refactor

--- geci_plots/plot_kernel_density_gls.py
+++ geci_plots/plot_kernel_density_gls.py
@@ -122,7 +122,7 @@ def _plot_kernel_density(gls_data, global_shapefile_data_path, path_rose_wind, s
     ax.xaxis.set_major_formatter(ticker.FormatStrFormatter("%d°"))
     ax.yaxis.set_major_formatter(ticker.FormatStrFormatter("%d°"))
 
-    plot_windrose(path_rose_wind, fig)
+    fig = plot_windrose(path_rose_wind, fig)
     return ax
 
 
@@ -135,6 +135,7 @@ def plot_windrose(path_rose_wind, fig):
     img_x = 1150
     img_y = 730
     fig.figimage(resized_rose_wind, img_x, img_y, zorder=100)
+    return fig
 
 
 def plot_global_politic_division(global_shapefile_data_path, ax):
