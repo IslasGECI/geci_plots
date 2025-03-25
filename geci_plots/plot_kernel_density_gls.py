65add75f83ea4b81074935804ac0e47db3925c17
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-25 18:48:28 +0000

    ♻️  Refactor

--- geci_plots/plot_kernel_density_gls.py
+++ geci_plots/plot_kernel_density_gls.py
@@ -18,7 +18,7 @@ def adapt_gls_data(gls_data_path):
 
 
 def _plot_kernel_density_and_points(
-    gls_data, global_shapefile_data_path, path_rose_wind, selected_contour
+    gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
 ):
     gls_data_translated = translate_positive_longitudes(gls_data)
 
@@ -26,18 +26,20 @@ def _plot_kernel_density_and_points(
     format_plot(ax)
     plot_global_politic_division(global_shapefile_data_path, ax)
     plot_geographic_points(gls_data_translated)
-    plot_kernel_contour(gls_data_translated, selected_contour, 0.04)
+    plot_kernel_contour(gls_data_translated, selected_contour, bandwidth)
     plot_windrose(path_rose_wind, fig)
     return ax
 
 
-def _plot_kernel_density(gls_data, global_shapefile_data_path, path_rose_wind, selected_contour):
+def _plot_kernel_density(
+    gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
+):
     gls_data_translated = translate_positive_longitudes(gls_data)
 
     fig, ax = plt.subplots(figsize=(14.3, 10.4))
     format_plot(ax)
     plot_global_politic_division(global_shapefile_data_path, ax)
-    plot_kernel_contour(gls_data_translated, selected_contour, 0.04)
+    plot_kernel_contour(gls_data_translated, selected_contour, bandwidth)
     plot_windrose(path_rose_wind, fig)
     return ax
 
--- tests/test_plot_kernel_density_gls.py
+++ tests/test_plot_kernel_density_gls.py
@@ -7,6 +7,8 @@ from geci_plots.plot_kernel_density_gls import (
 import matplotlib as plt
 import pandas as pd
 
+bandwidth = 0.04
+
 
 def test_plot_kernel_density_and_gls_points():
     gls_data_path = "tests/data/gls_albatros_tests.csv"
@@ -16,7 +18,7 @@ def test_plot_kernel_density_and_gls_points():
 
     gls_data = pd.read_csv(gls_data_path)
     obtained = _plot_kernel_density_and_points(
-        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour
+        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
     )
     assert isinstance(obtained, plt.axes._axes.Axes)
 
@@ -29,7 +31,7 @@ def test_plot_kernel_density_and_points():
 
     gls_data = adapt_gls_data(gls_data_path)
     obtained = _plot_kernel_density_and_points(
-        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour
+        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
     )
     assert isinstance(obtained, plt.axes._axes.Axes)
     assert len(obtained.collections) == 3
@@ -44,7 +46,7 @@ def test_plot_kernel_density():
 
     gls_data = adapt_gls_data(gls_data_path)
     obtained = _plot_kernel_density(
-        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour
+        gls_data, global_shapefile_data_path, path_rose_wind, selected_contour, bandwidth
     )
     plt.pyplot.savefig("kernel.png")
     assert isinstance(obtained, plt.axes._axes.Axes)
