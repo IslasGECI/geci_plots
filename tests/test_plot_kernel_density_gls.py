32bd0ad7f1860793feedce53f3ad16ed2079c446
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-25 16:41:36 +0000

    ♻️  Refactor

--- tests/test_plot_kernel_density_gls.py
+++ tests/test_plot_kernel_density_gls.py
@@ -31,3 +31,5 @@ def test_plot_kernel_density():
         gls_data, global_shapefile_data_path, path_rose_wind, selected_contour
     )
     assert isinstance(obtained, plt.axes._axes.Axes)
+    assert len(obtained.collections) == 3
+    assert len(obtained.lines) == 1
