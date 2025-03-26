14b990c70933eb07790f656069e6ab8c3e805136
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-26 21:19:11 +0000

    🛑🧪 Fail tests

--- tests/test_plot_kernel_density_gls.py
+++ tests/test_plot_kernel_density_gls.py
@@ -70,4 +70,4 @@ def test_format_plot():
     fig, ax = plt.pyplot.subplots(figsize=(14.3, 10.4))
     gps_data = adapt_geographic_data(gps_data_path)
     format_plot(ax, gps_data)
-    assert ax.get_ylim()[1] < 65
+    assert ax.get_ylim()[1] < 55
