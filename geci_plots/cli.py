0825eef66b56661ba09a1a23bbaab494f3b8bcf4
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-26 20:24:28 +0000

    ✅ Pass tests

--- geci_plots/cli.py
+++ geci_plots/cli.py
@@ -1,6 +1,7 @@
 from geci_plots.plot_kernel_density_gls import (
     adapt_geographic_data,
     _plot_geographic_points,
+    _plot_geographic_points_by_trip,
     _plot_kernel_density_and_points,
     _plot_kernel_density,
 )
@@ -52,6 +53,18 @@ def plot_geographic_points(
 ):
     geographic_data = adapt_geographic_data(geographic_data_path)
     _plot_geographic_points(geographic_data, global_shapefile_data_path, path_rose_wind)
+    plt.savefig(result_map_path)
+
+
+@cli.command()
+def plot_geographic_points_by_trip(
+    geographic_data_path: str = typer.Option(),
+    global_shapefile_data_path: str = typer.Option(),
+    path_rose_wind: str = typer.Option(),
+    result_map_path: str = typer.Option(),
+):
+    geographic_data = adapt_geographic_data(geographic_data_path)
+    _plot_geographic_points_by_trip(geographic_data, global_shapefile_data_path, path_rose_wind)
     plt.savefig(result_map_path)
 
 
