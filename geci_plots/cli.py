38eceabbb8a2d1b7ec3fea12c11e71edbb48ec04
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2025-03-28 21:46:15 +0000

    ✅ Pass tests

--- geci_plots/cli.py
+++ geci_plots/cli.py
@@ -6,9 +6,11 @@ from geci_plots.plot_kernel_density_gls import (
     _plot_kernel_density_and_points,
     adapt_geographic_data,
 )
+from geci_plots.boxplots import create_box_plot, create_box_plot_data_from_columns
 import geci_plots as gp
-import typer
 import matplotlib.pyplot as plt
+import pandas as pd
+import typer
 
 cli = typer.Typer()
 
@@ -79,6 +81,20 @@ def plot_geographic_points_by_vessel(
     geographic_data = adapt_geographic_data(geographic_data_path)
     _plot_geographic_points_by_vessel(geographic_data, global_shapefile_data_path, path_rose_wind)
     plt.savefig(result_map_path)
+
+
+@cli.command()
+def boxplot(
+    data_path: str = typer.Option(),
+    columns_of_interest: str = typer.Option(),
+    boxplot_path: str = typer.Option(),
+):
+    summary_data = pd.read_csv(data_path)
+    columns = columns_of_interest.split(",")
+    print(columns)
+    data_for_boxplot = create_box_plot_data_from_columns(summary_data, columns)
+    create_box_plot(data_for_boxplot)
+    plt.savefig(boxplot_path)
 
 
 @cli.command()
