9ab7ea7874f3f8813a4fb924d2f217ed582c011b
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-11 21:04:45 +0000

    ✅ Pass tests

--- geci_plots/process_cameras_data.py
+++ geci_plots/process_cameras_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def summarize_monthly_cameras_effort_and_captures(cameras_data, begin_date, final_date):
     cameras_data["Date"] = pd.to_datetime(cameras_data["Date"])
-    cameras_data = cameras_data.set_index(["Date"])
-    cameras_data = cameras_data.resample("MS").sum()
+    cameras_data = cameras_data.set_index(["Date"]).sort_index()
     cameras_data = select_date_interval(cameras_data, begin_date, final_date)
+    cameras_data = cameras_data.resample("MS").sum()
     return cameras_data
