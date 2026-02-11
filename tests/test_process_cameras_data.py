2999318149f079ce684bf520055b07116722ff65
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-11 21:09:20 +0000

    🛑🧪 Fail tests

--- tests/test_process_cameras_data.py
+++ tests/test_process_cameras_data.py
@@ -1,10 +1,20 @@
-from geci_plots.process_cameras_data import summarize_monthly_cameras_effort_and_captures
+from geci_plots.process_cameras_data import (
+    summarize_monthly_cameras_effort_and_captures,
+    _plot_monthly_cameras_effort_and_captures,
+)
 
 import pandas as pd
+import matplotlib as plt
+
+camera_data = pd.read_csv("tests/data/weekly_cameras_effort.csv")
+
+
+def test_plot_monthly_cameras_effort_and_captures():
+    obtained_ax = _plot_monthly_cameras_effort_and_captures(camera_data)
+    assert isinstance(obtained_ax, plt.axes._axes.Axes)
 
 
 def test_summarize_monthly_cameras_effort_and_captures():
-    camera_data = pd.read_csv("tests/data/weekly_cameras_effort.csv")
     begin_date = "2025-01-01"
     end_date = "2026-01-04"
     obtained = summarize_monthly_cameras_effort_and_captures(camera_data, begin_date, end_date)
