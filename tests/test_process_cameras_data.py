aa6e13cf2f71dc9ad82e4f97f65f4412a28e07ce
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-02-11 21:15:36 +0000

    🛑🧪 Fail tests

--- tests/test_process_cameras_data.py
+++ tests/test_process_cameras_data.py
@@ -7,16 +7,16 @@ import pandas as pd
 import matplotlib as plt
 
 camera_data = pd.read_csv("tests/data/weekly_cameras_effort.csv")
+begin_date = "2025-01-01"
+end_date = "2026-01-04"
 
 
 def test_plot_monthly_cameras_effort_and_captures():
-    obtained_ax = _plot_monthly_cameras_effort_and_captures(camera_data)
+    obtained_ax = _plot_monthly_cameras_effort_and_captures(camera_data, begin_date, end_date)
     assert isinstance(obtained_ax, plt.axes._axes.Axes)
 
 
 def test_summarize_monthly_cameras_effort_and_captures():
-    begin_date = "2025-01-01"
-    end_date = "2026-01-04"
     obtained = summarize_monthly_cameras_effort_and_captures(camera_data, begin_date, end_date)
     expected_nrows = 4
     assert len(obtained) == expected_nrows
