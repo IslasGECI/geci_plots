5e889c1404e1047cbf39b78b70e70e6fbc388926
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-03 16:55:09 +0000

    🛑🧪 Fail tests

--- tests/test_process_effort_and_captures_data.py
+++ tests/test_process_effort_and_captures_data.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 def test_process_effort_and_captures_data_by_zone():
     effort_captures_data = pd.read_csv("tests/data/monthly_effort_and_captures_by_zone.csv")
+    starting_date = "2026-01-01"
+    ending_date = "2026-03-31"
     obtained = process_effort_and_captures_data_by_zone(effort_captures_data)
     expected_number_of_months = 3
     expected_number_of_column_zones = 3
