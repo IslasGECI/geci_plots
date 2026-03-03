65ef2e111ea595d4cd520b1664b348ae8eade4fe
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-03-03 18:06:36 +0000

    🆙 upgrade minor number version

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -272,5 +272,5 @@ def test_boxplot():
 def test_version():
     result = runner.invoke(cli, ["version"])
     assert result.exit_code == 0
-    expected_version = "0.9.0"
+    expected_version = "0.10.0"
     assert expected_version in result.stdout
