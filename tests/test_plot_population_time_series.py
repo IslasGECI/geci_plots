9617a3f55d8aa1a93f52ee5a2c6c41f14af83d85
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-04-06 21:08:20 +0000

    🛑🧪 Fail tests

--- tests/test_plot_population_time_series.py
+++ tests/test_plot_population_time_series.py
@@ -75,3 +75,4 @@ def test_plot_population_time_series():
     expected_ylabel = "Number of individuals"
     assert obtained.get_ylabel() == expected_ylabel
     assert obtained.get_xticklabels()[0].get_text() == "Jan 2021"
+    assert "." in obtained.get_lines()[0].get_marker()
