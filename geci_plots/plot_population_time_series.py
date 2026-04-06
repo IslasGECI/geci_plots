12fbc006a1f3be41620081748e71084438f0926d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-04-06 20:34:59 +0000

    ✅ Pass tests

--- geci_plots/plot_population_time_series.py
+++ geci_plots/plot_population_time_series.py
@@ -1,7 +1,17 @@
 from geci_plots import plt
+import matplotlib.dates as mdates
+import pandas as pd
 
 
 def plot_population_time_series(data):
     fig, ax = plt.subplots(figsize=(14.3, 10.4))
+
+    data["Date"] = pd.to_datetime(data["Date"])
+
+    ax.plot(data["Date"], data["Captures"])
+
     plt.ylabel("Number of individuals")
+    ax.xaxis.set_major_locator(mdates.MonthLocator())
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%Y-%m"))
+
     return ax
