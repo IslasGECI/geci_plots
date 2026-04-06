26ba2387a84ff73c4d9b464248be983913739709
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-04-06 21:40:15 +0000

    ✅ Pass tests

--- geci_plots/plot_population_time_series.py
+++ geci_plots/plot_population_time_series.py
@@ -15,7 +15,7 @@ def plot_population_time_series(data):
     ax.xaxis.set_major_locator(mdates.MonthLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
     loc, labels = plt.xticks()
-    plt.xticks(loc[1:-1], labels[1:-1], rotation=90, fontsize=20)
+    plt.xticks(loc[1:-1], labels[1:-1], rotation=90, fontsize=fontsize)
     plt.legend(fontsize="xx-large")
 
     return ax
