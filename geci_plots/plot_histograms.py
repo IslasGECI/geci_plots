3b0449bd60a6e6ecb0fc41b5b5961c2c79c92b2e
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-04-07 17:38:29 +0000

    ✅ Pass tests

--- geci_plots/plot_histograms.py
+++ geci_plots/plot_histograms.py
@@ -1,8 +1,11 @@
 from geci_plots.geci_plots import geci_plot
 
+import matplotlib.pyplot as plt
+
 
 def plot_histogram_with_limits(x, bins, limits=[], plot_options={}, lines_options={}):
     _, ax = geci_plot()
+    plt.xlabel(plot_options["label"])
     ax.hist(x, bins=bins, **plot_options)
     for lines in limits:
         ax.axvline(x=lines, **lines_options)
