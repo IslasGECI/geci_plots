9da195db633de8483990c4231a60075206e36630
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-04-07 17:44:51 +0000

    ♻️  Refactor

--- tests/test_plot_histograms.py
+++ tests/test_plot_histograms.py
@@ -9,9 +9,14 @@ def test_plot_histogram_with_limits():
     x = data[column_name]
     plot_options = {"label": column_name}
     limits = [47698]
-    obtained = plot_histogram_with_limits(x, None, limits=limits, plot_options=plot_options)
+    color_line = "r"
+    lines_options = {"color": color_line}
+    obtained = plot_histogram_with_limits(
+        x, None, limits=limits, plot_options=plot_options, lines_options=lines_options
+    )
     assert isinstance(obtained, plt.axes._axes.Axes)
 
     assert obtained.get_xlabel() == column_name
     plt.pyplot.savefig("histogram.png")
     assert obtained.get_lines()[0].get_data()[0][0] == limits[0]
+    assert obtained.get_lines()[0].get_color() == color_line
