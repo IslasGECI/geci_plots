c20c5c08cdf4837e554bd7433b9e8b64966d6f79
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-04-07 17:36:43 +0000

    🛑🧪 Fail tests

--- tests/test_plot_histograms.py
+++ tests/test_plot_histograms.py
@@ -6,7 +6,9 @@ import pandas as pd
 def test_plot_histogram_with_limits():
     data = pd.read_csv("tests/data/monthly_data.csv")
     column_name = "Effort"
-    obtained = plot_histogram_with_limits(data[column_name], None)
+    x = data[column_name]
+    plot_options = {"label": column_name}
+    obtained = plot_histogram_with_limits(x, None, plot_options=plot_options)
     assert isinstance(obtained, plt.axes._axes.Axes)
 
     assert obtained.get_xlabel() == column_name
