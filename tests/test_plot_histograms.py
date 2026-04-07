3ff31ea94e7f00e2c855106e54b5a16894d47c6d
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-04-07 17:42:52 +0000

    ♻️  Refactor

--- tests/test_plot_histograms.py
+++ tests/test_plot_histograms.py
@@ -8,7 +8,10 @@ def test_plot_histogram_with_limits():
     column_name = "Effort"
     x = data[column_name]
     plot_options = {"label": column_name}
-    obtained = plot_histogram_with_limits(x, None, plot_options=plot_options)
+    limits = [47698]
+    obtained = plot_histogram_with_limits(x, None, limits=limits, plot_options=plot_options)
     assert isinstance(obtained, plt.axes._axes.Axes)
 
     assert obtained.get_xlabel() == column_name
+    plt.pyplot.savefig("histogram.png")
+    assert obtained.get_lines()[0].get_data()[0][0] == limits[0]
