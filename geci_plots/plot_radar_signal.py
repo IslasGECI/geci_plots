fb258c9c34ec076e246add460dc0042377a27252
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-06-04 20:10:19 +0000

    ✅ Pass tests

--- geci_plots/plot_radar_signal.py
+++ geci_plots/plot_radar_signal.py
@@ -11,4 +11,13 @@ def plot_radar_signal_geographic_points(
     radar_signal_points_data, global_shapefile_data_path, path_rose_wind
 ):
     fig, ax = plt.subplots(figsize=(14.3, 10.4))
+    format_plot(ax, radar_signal_points_data)
+    plot_global_politic_division(global_shapefile_data_path, ax)
+    plt.scatter(
+        radar_signal_points_data["longitude"],
+        radar_signal_points_data["latitude"],
+        c=radar_signal_points_data["radar_signal"],
+        alpha=0.3,
+    )
+    plot_windrose(path_rose_wind, fig)
     return ax
