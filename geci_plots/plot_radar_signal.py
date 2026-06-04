9b5cf4636795eb78fc4d247e5e701f1712c8fe9a
Ciencia de Datos • GECI <ciencia.datos@islas.org.mx>
2026-06-04 19:47:36 +0000

    ✅ Pass tests

---
+++ geci_plots/plot_radar_signal.py
@@ -0,0 +1,14 @@
+from geci_plots import plt
+from geci_plots.plot_kernel_density_gls import (
+    format_plot,
+    plot_global_politic_division,
+    plot_geographic_points,
+    plot_windrose,
+)
+
+
+def plot_radar_signal_geographic_points(
+    radar_signal_points_data, global_shapefile_data_path, path_rose_wind
+):
+    fig, ax = plt.subplots(figsize=(14.3, 10.4))
+    return ax
